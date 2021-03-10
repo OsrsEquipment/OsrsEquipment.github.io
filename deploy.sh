fbe377e4882923d67510c8ab9d2402c94bf8ef09
Giovani Vercauteren <giovani.vercauteren@gmail.com>
2021-03-10 10:25:18 +0100

    updates for new repo

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:toolscape/toolscape.github.io.git master:gh-pages
+git push -f git@github.com:OsrsEquipment/OsrsEquipment.github.io.git master:gh-pages
 cd -
