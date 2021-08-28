b4fc8f12eef83ee1d7699d7ad7891da5a4247b72
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2021-08-28 20:54:39 +0200

    Data management

--- src/components/OsrsNavbar.vue
+++ src/components/OsrsNavbar.vue
@@ -16,6 +16,14 @@
     </router-link>
     <router-link
       class="osrs-navbar-link"
+      :to="{ name: 'DataManagement' }"
+      active-class="osrs-navbar-active-link"
+      exact
+    >
+      Data
+    </router-link>
+    <router-link
+      class="osrs-navbar-link"
       :to="{ name: 'About' }"
       active-class="osrs-navbar-active-link"
       exact
--- src/views/DataManagement.vue
+++ src/views/DataManagement.vue
@@ -1,8 +1,47 @@
 <template>
   <v-container>
-    <v-btn @click="checkForUpdates">
-      Check for updates
-    </v-btn>
+    <v-row>
+      <v-col>
+        <div
+          v-if="checking"
+          class="in-progress-container"
+        >
+          <span>Checking for updates</span>
+          <v-progress-circular
+            class="my-2"
+            indeterminate
+          />
+        </div>
+        <div
+          v-else-if="!checking && !updating && updateRequired"
+          class="need-updates-container"
+        >
+          <span>{{ updateData.length }} databases need updating</span>
+          <v-btn
+            class="my-2"
+            @click="startUpdate"
+          >
+            update
+          </v-btn>
+        </div>
+        <div
+          v-else-if="updating"
+          class="need-updates-container"
+        >
+          <span>Updating</span>
+          <v-progress-circular
+            class="my-2"
+            indeterminate
+          />
+        </div>
+        <div
+          v-else
+          class="no-updates-container"
+        >
+          <span>No updates required</span>
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -13,16 +52,51 @@ export default {
   name: 'DataManager',
   data() {
     return {
-      contents: [],
-      content: undefined,
+      checking: false,
+      updating: false,
+      updateData: [],
     };
+  },
+  computed: {
+    updateRequired() {
+      return this.updateData && this.updateData.length > 0;
+    },
+  },
+  mounted() {
+    this.checkForUpdates();
   },
   methods: {
     async checkForUpdates() {
-      await DataManager.checkForUpdates().then((updates) => {
-        DataManager.update(updates);
-      });
+      try {
+        this.checking = true;
+        this.updateData = await DataManager.checkForUpdates();
+      } finally {
+        this.checking = false;
+      }
+    },
+    async startUpdate() {
+      try {
+        this.updating = true;
+        await DataManager.update(this.updateData);
+        this.updateData = await DataManager.checkForUpdates();
+      } finally {
+        this.updating = false;
+      }
     },
   },
 };
 </script>
+
+<style scoped>
+.no-updates-container,
+.need-updates-container,
+.in-progress-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 200px;
+  width: 100%;
+  font-size: 24px;
+}
+</style>
