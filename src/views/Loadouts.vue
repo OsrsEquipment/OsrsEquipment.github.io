a41abf7192989dc33bd66a7ed09e783187ca0007
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2021-08-29 21:12:49 +0200

    Make data aquisition requirement obvious

--- src/views/Loadouts.vue
+++ src/views/Loadouts.vue
@@ -1,21 +1,51 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>
+      <v-col v-if="hasValidData">
         <loadout-manager/>
+      </v-col>
+      <v-col v-else>
+        <div class="no-data-disclaimer">
+          <span>You have no valid data</span>
+          <v-btn
+            class="my-2"
+            to="data"
+          >
+            Manage data
+          </v-btn>
+        </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import LoadoutManager from '../components/loadout-manager/LoadoutManager.vue';
 
 export default {
   name: 'Loadouts',
   components: { LoadoutManager },
+  computed: {
+    ...mapState({
+      equipment: (state) => state.equipment.list,
+      monsters: (state) => state.monsters.list,
+    }),
+    hasValidData() {
+      return this.equipment && this.equipment.length > 0
+        && this.monsters && this.monsters.length > 0;
+    },
+  },
 };
 </script>
 
 <style scoped>
+.no-data-disclaimer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 200px;
+  font-size: 24px;
+}
 </style>
